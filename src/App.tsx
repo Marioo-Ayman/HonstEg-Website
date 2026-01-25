0e87dfb2657bd537bb06540135d3cfc426b311c9
Marioo-Ayman <96667299+Marioo-Ayman@users.noreply.github.com>
2026-01-25 19:11:04 +0200

    try lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import Main from '@/pages/Main';
 import Apply from '@/pages/Apply';
 import './App.css'
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 function App() {
 
-
+const Main = lazy(() => import("@/pages/Main"));
+const Footer = lazy(() => import("@/components/Footer"));
   return (
     <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Navbar />
         <Routes>
           <Route path="*" element={<Main />} />
           <Route path="/invest" element={<Apply />} />
         </Routes>
       <Footer />
+    </Suspense>
     </>
   )
 }
